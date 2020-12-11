9cf4916c3c0f2bde0781746b4d628bf3352c85ba
Nadav Nini <nadavni12@gmail.com>
2020-12-11 20:24:17 +0200

    Added Theme Model, close #5

---
+++ CodeNameLogic/Theme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace CodeNameLogic
+{
+    class Theme
+    {
+        public Color ThemeColor { get; set; }
+
+        public string ThemeDecorationImage { get; set; }
+
+        Theme(Color color) => (ThemeColor) = (color);
+    }
+}
