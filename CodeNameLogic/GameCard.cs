e2f1aaeb5d35ccfae7c8312b615fdfaddd40ffa0
Nadav Nini <nadavni12@gmail.com>
2020-12-11 20:09:19 +0200

    Added GameCard class, close #3

---
+++ CodeNameLogic/GameCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeNameLogic
+{
+    public class GameCard : Card
+    {
+        public bool IsActive { get; set; }
+
+        public GameCard(uint id = 0, string word = "") : 
+            base(id, word)
+        {
+
+        }
+    }
+}
