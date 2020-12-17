ce6b39ddca7bb19aab40c9db00717de9f5bc41c6
Nadav Nini <nadavni12@gmail.com>
2020-12-17 23:34:18 +0200

    I have added Board Class. I have encountered an issue implementing GetBoardDescriptor so I created issue #17

---
+++ CodeNameLogic/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeNameLogic
+{
+    public class Board
+    {
+        public delegate BoardDescriptor GetBoardDescriptorDelegate();
+
+        public GameCard[,] GameCards { get; set; }
+
+        public BoardDescriptor BoardCardsDescriptor { get; set; }
+
+        public GameTheme BoardTheme { get; set; }
+
+        public Board(uint rows, uint columns)
+        {
+            GameCards = new GameCard[rows, columns];
+            BoardCardsDescriptor = new BoardDescriptor(rows, columns);
+            BoardTheme = new GameTheme();
+        }
+    }
+}
