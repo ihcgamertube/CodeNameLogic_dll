d2e545b59ebb46e4f97ef6e12dd159eeb7eb05b0
Nadav Nini <nadavni12@gmail.com>
2020-12-17 22:45:18 +0200

    Create Board Descriptor, close #8

---
+++ CodeNameLogic/BoardDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeNameLogic
+{
+    public class BoardDescriptor
+    {
+        public CardRole[,] CardRoles { get; set; }
+
+        public BoardDescriptor(uint rows, uint colummns)
+        {
+            CardRoles = new CardRole[rows, colummns];
+        }
+
+        // return card role as reference via row & column
+        public ref CardRole SetCardRole(uint row, uint column)
+        { 
+            return ref CardRoles[row, column];
+        }
+
+    }
+}
