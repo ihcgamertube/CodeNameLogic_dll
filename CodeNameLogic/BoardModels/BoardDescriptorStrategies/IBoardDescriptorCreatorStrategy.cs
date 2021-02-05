fc4198494b0a98c8a248e36ed0b7a9eaf74b6082
ihcgamertube <isrgamertubev2@gmail.com>
2021-02-05 22:54:22 +0200

    added randomizer strategy for board descriptor creator

---
+++ CodeNameLogic/BoardModels/BoardDescriptorStrategies/IBoardDescriptorCreatorStrategy.cs
@@ -0,0 +1,12 @@
+﻿using CodeNameLogic.CardModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeNameLogic.BoardModels.BoardDescriptorStrategies
+{
+    public interface IBoardDescriptorCreatorStrategy
+    {
+        public CardRole[,] GenerateCardRoles(int rows, int collumns, int teamsCounts); 
+    }
+}
